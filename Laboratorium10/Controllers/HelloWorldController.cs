0f9bd84949e816731a66dad07cd352155fb2f567
Dominik Filipczuk <dominik66670@hotmail.com>
2023-06-02 13:37:14 +0200

    Dodanie podstawowego kontrolera Dodaj pliki projektów.

---
+++ Laboratorium10/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Laboratorium10.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "To jest domyślna akcja";
+        }
+        public string Welcome(string name, int ID = 1) 
+        {
+            return HtmlEncoder.Default.Encode($"Witaj {name}, ID: {ID}");
+        }
+    }
+}
